6cb4f2391102b2f2a101910fcbba0bd9295f1c1a
ytsoni <34114756+ytsoni@users.noreply.github.com>
2024-09-08 12:03:21 +0530

    Add dynamic programming solution for evaluating different ways to add parentheses in an expression

--- src/DynamicProgramming/DifferentWaysToAddParenthesis.java
+++ src/DynamicProgramming/DifferentWaysToAddParenthesis.java
@@ -1,5 +1,70 @@
 package DynamicProgramming;
 
-public class DifferentWaysToAddParenthesis {
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
+public class DifferentWaysToAddParenthesis {
+    public List<Integer> solve(String exp){
+        List<List<List<Integer>>> dp = new ArrayList<>();
+        for(int i = 0; i<= exp.length(); i++){
+            dp.add(i, new ArrayList<>());
+            for(int j = 0; j<= exp.length(); j++){
+                dp.get(i).add(j, new ArrayList<>());
+            }
+        }
+        return getResultOfExpression(exp, 0, exp.length(), dp);
+    }
+
+
+    private List<Integer> getResultOfExpression(String exp, int start, int end, List<List<List<Integer>>> dp){
+        if((dp.get(start).get(end).size() > 0)) return dp.get(start).get(end);
+
+        int num = 0;
+        boolean isOperatorPresent = false;
+        List<Integer> res = new ArrayList<>();
+
+        for(int i = start; i< end; i++){
+            char ch = exp.charAt(i);
+
+            if(!isOperator(ch)){
+                num = num*10 + (ch - '0');
+                continue;
+            }
+
+            isOperatorPresent = true;
+
+            List<Integer> left = getResultOfExpression(exp, start, i, dp);
+            List<Integer> right = getResultOfExpression(exp, i+1, end, dp);
+
+            for(int a : left){
+                for(int b : right){
+                    res.add(applyOperator(a, b, ch));
+                }
+            }
+
+        }
+
+        if(!isOperatorPresent) res.add(num);
+
+        // System.out.println(dp.size() + " start"+ start + " end"+end);
+
+        dp.get(start).get(end).addAll(res);
+
+        return res;
+    }
+
+    private boolean isOperator(char ch){
+        return ch == '+' || ch == '-' || ch == '*';
+    }
+
+    private int applyOperator(int a, int b, char op){
+        switch(op){
+            case '+': return a+b;
+            case '-': return a-b;
+            case '*': return a*b;
+        }
+        return 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import DynamicProgramming.DifferentWaysToAddParenthesis;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        DifferentWaysToAddParenthesis differentWaysToAddParenthesis = new DifferentWaysToAddParenthesis();
+        System.out.println(differentWaysToAddParenthesis.solve("2*3-4*5"));
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
